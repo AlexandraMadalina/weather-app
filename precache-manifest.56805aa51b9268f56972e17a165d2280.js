78096bb4390d2d0cb759b1581dbd395ffaa112eb
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-07-03 12:32:28 +0200

    Updates

---
+++ precache-manifest.56805aa51b9268f56972e17a165d2280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5666a0a0311aa2295e0d31b177405739",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "6da5d964182790b42ca8",
+    "url": "/Todo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "64016d5678b1d5e6b01a",
+    "url": "/Todo/static/css/main.f590c241.chunk.css"
+  },
+  {
+    "revision": "6da5d964182790b42ca8",
+    "url": "/Todo/static/js/2.ba7a277f.chunk.js"
+  },
+  {
+    "revision": "64016d5678b1d5e6b01a",
+    "url": "/Todo/static/js/main.3b8892b4.chunk.js"
+  },
+  {
+    "revision": "6fab73d75525f9d6064a",
+    "url": "/Todo/static/js/runtime~main.ee49a031.js"
+  }
+]);
